e3f571a45e22409673505d0307e7466fde14b215
Kevin Jensen <kevin@riskcanvas.com>
2023-11-17 13:13:17 -0700

    sync

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(80, () => {
+  console.log('listening on *:80');
 });
