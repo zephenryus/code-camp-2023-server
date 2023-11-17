8eaeee0b834030ce2b96c1ceb4e905207dcaf77c
Kevin Jensen <kevin@riskcanvas.com>
2023-11-17 15:07:33 -0700

    sync

--- server.js
+++ server.js
@@ -4,12 +4,6 @@ const path = require('path');
 
 const app = express();
 const port = 8080;
-const io = socketIo(server, {
-  cors: {
-    origin: "http://localhost:4200",
-    methods: ["GET", "POST"]
-  }
-});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
