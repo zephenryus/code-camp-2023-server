7c3780702346cfdc00f9deae6aab6f048f27050c
Kevin Jensen <kevin@riskcanvas.com>
2023-11-17 19:22:38 -0700

    Added meme search

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const socketIo = require('socket.io');
+const axios = require('axios');
+const https = require('https');
 const path = require('path');
 
 const app = express();
 const port = 8080;
+const memesData = require('./public/assets/images.json');
 
 app.use(express.static(path.join(__dirname, 'public')));
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
@@ -14,6 +20,20 @@ app.get("/", (req, res) => {
 app.use('/new-game', express.static(path.join(__dirname, 'public/app')));
 app.use('/join-game', express.static(path.join(__dirname, 'public/app')));
 
+function searchMemes (query) {
+  return memesData.filter(meme => meme.name.toLowerCase().includes(query.toLowerCase()));
+}
+
+// Search endpoint
+app.get('/search-memes', (req, res) => {
+  const query = req.query.query;
+  if (!query) {
+    return res.status(400).send('Search query is required');
+  }
+  const results = searchMemes(query);
+  res.json(results);
+});
+
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${ port }`);
 });
